c66b55f1d0380bb1224a80fd41f26cce8979f8e8
Guy Moyo <guymoyo@yahoo.fr>
2018-06-12 11:37:37 +0200

    add resource

--- src/main/java/com/cergenerator/service/helper/CertificateUtils.java
+++ src/main/java/com/cergenerator/service/helper/CertificateUtils.java
@@ -1,21 +1,16 @@
 package com.cergenerator.service.helper;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.URL;
 import java.security.KeyPair;
 import java.security.PrivateKey;
 import java.security.Security;
 import java.security.cert.X509Certificate;
-import java.util.ArrayList;
-import java.util.List;
 
-import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.openssl.PEMKeyPair;
 import org.bouncycastle.openssl.PEMParser;
@@ -25,47 +20,16 @@ import com.nimbusds.jose.util.X509CertUtils;
 
 public class CertificateUtils {
 
-	public static X509Certificate[] getCertificates(String folderName) {
-
-		List<X509Certificate> listCert = new ArrayList<>();
-
-		ClassLoader loader = Thread.currentThread().getContextClassLoader();
-		URL url = loader.getResource(folderName);
-		if(url == null) {
-			return new X509Certificate[] {};
-		}
-		String path = url.getPath();
-		File[] files = new File(path).listFiles();
-
-		for (File file : files) {
-			
-			try {
-				byte[] bytesArray = FileUtils.readFileToByteArray(file);
-				X509Certificate cert = X509CertUtils.parse(bytesArray);
-				if (cert != null) {
-					listCert.add(cert);
-				}
-			} catch (IOException e1) {
-				
-				e1.printStackTrace();
-			}
-
-		}
-		return listCert.toArray(new X509Certificate[listCert.size()]);
-	}
 
 	public static String getCertificateByName(String filename) {
 
 		ClassLoader loader = Thread.currentThread().getContextClassLoader();
-		URL url = loader.getResource("certificates/" + filename);
-		String path = url.getPath();
-		File file = new File(path);
-
-		if (file.exists()) {
-
+		InputStream is = loader.getResourceAsStream("certificates/" + filename);
+		
+		if (is != null) {
 			try {
-				byte[] bytesArray = FileUtils.readFileToByteArray(file);
-				X509Certificate cert = X509CertUtils.parse(bytesArray);
+				byte[] bytes = IOUtils.toByteArray(is);
+				X509Certificate cert = X509CertUtils.parse(bytes);
 				String encodeCert = X509CertUtils.toPEMString(cert);
 				return encodeCert;
 			} catch (IOException e) {
@@ -80,15 +44,12 @@ public class CertificateUtils {
 	public static X509Certificate getCertificate(String filename) {
 
 		ClassLoader loader = Thread.currentThread().getContextClassLoader();
-		URL url = loader.getResource("certificates/" + filename);
-		String path = url.getPath();
-		File file = new File(path);
-
-		if (file.exists()) {
-
+		InputStream is = loader.getResourceAsStream("certificates/" + filename);
+		
+		if (is != null) {
 			try {
-				byte[] bytesArray = FileUtils.readFileToByteArray(file);
-				X509Certificate cert = X509CertUtils.parse(bytesArray);
+				byte[] bytes = IOUtils.toByteArray(is);
+				X509Certificate cert = X509CertUtils.parse(bytes);
 				return cert;
 			} catch (IOException e) {
 				e.printStackTrace();
